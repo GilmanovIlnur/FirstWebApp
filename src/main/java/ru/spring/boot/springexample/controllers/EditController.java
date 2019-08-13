9bbd00ace90ee88f65fcc5b8f80092a6bf73e3c1
GilmanovIlnur <qazxsw777>
2019-08-13 23:55:48 +0300

    Обработка редактирования пользователя админом.

--- src/main/java/ru/spring/boot/springexample/controllers/EditController.java
+++ src/main/java/ru/spring/boot/springexample/controllers/EditController.java
@@ -1,7 +1,9 @@
 package ru.spring.boot.springexample.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -11,8 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.spring.boot.springexample.forms.EditForm;
 import ru.spring.boot.springexample.forms.PasswordEditForm;
+import ru.spring.boot.springexample.models.Role;
 import ru.spring.boot.springexample.models.User;
 import ru.spring.boot.springexample.repositories.UserRepo;
+import ru.spring.boot.springexample.security.details.UserDetailsImpl;
+
+import java.util.Map;
+import java.util.Set;
 
 
 @Controller
@@ -24,15 +31,25 @@ public class EditController {
     UserRepo userRepo;
 
     @GetMapping("/profile/users/{id}")
-    public String userEdit(@PathVariable("id") User user, ModelMap modelMap){
-        modelMap.addAttribute("user",user);
+    public String userEdit(@PathVariable("id") User editUser, ModelMap modelMap,
+                           Authentication authentication){
+        UserDetailsImpl details = (UserDetailsImpl) authentication.getPrincipal();
+        modelMap.addAttribute("roles", Role.values());
+        if (details.getUser().isAdmin() && !editUser.equals(details.getUser())){
+            modelMap.addAttribute("isAdmin", true);
+        }
+
+        modelMap.addAttribute("user",editUser);
         return "userEdit";
     }
 
     @PostMapping("/profile/users/{id}")
-    public String edit(@PathVariable("id") User user, EditForm editForm){
+    public String edit(@PathVariable("id") User user, EditForm editForm,
+                       @RequestParam Set<Role> roles){
+
         user.setName(editForm.getName());
         user.setLastName(editForm.getLastName());
+        user.setRoles(roles);
         userRepo.save(user);
 
         return "redirect:/profile/users";
